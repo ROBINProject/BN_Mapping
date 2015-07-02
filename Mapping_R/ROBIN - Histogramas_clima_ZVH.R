3ed78cef91822728a0efd6c72b371d0b05a0e5b8
MiguelEq <equihuam@gmail.com>
2015-07-02 15:48:37 -0500

    Opción de cambiar el tamaño de las clases en el hist

--- Mapping_R/ROBIN - Histogramas_clima_ZVH.R
+++ Mapping_R/ROBIN - Histogramas_clima_ZVH.R
@@ -9,18 +9,18 @@ library (ggplot2)
 library(reshape2)
 
 # Función definida para generar histogramas de una variable en el ciclo anual y guardarlos
-histogramas.clima <- function (vars, archivo, ubicación)
+histogramas.clima <- function (vars, intervalo, archivo, ubicación)
 {
   # Genera una tabla de datos con dos columnas que etiquetan 
   # los datos según el nombre de la columna de orígen
   mdat <- melt(climat.2.df@data[,vars]) 
   
   graf <- ggplot(mdat, aes(value)) +
-    geom_histogram(aes(y=..count..), binwidth=2, colour='black', fill='skyblue') + 
+    geom_histogram(aes(y=..count..), binwidth=intervalo, colour='black', fill='skyblue') + 
     geom_density() + facet_wrap(~variable, scales="free")
   
   # Save the plot in GD sync structure
-  ggsave(archivo, path = dir.ref, graf, width = 22, height = 17, units = "in" )
+  ggsave(archivo, path = dir.ref, graf, width = 22, height = 17, units = "in")
   
   return (graf)
 }
@@ -35,11 +35,11 @@ dir <- ubica.trabajo(equipo="miguel_esc")
 dir.ref <- paste(dir$GIS.in, "/2004/clima", sep="")
 
 # Precipitación
-graf <- histogramas.clima (pp.vars, "ZVH_pp_hist.pdf", dir.ref)
+graf <- histogramas.clima (pp.vars, 2, "ZVH_pp_hist.pdf", dir.ref)
 
 # Temperatura máxima
-graf <- histogramas.clima (tmax.vars, "ZVH_tmax_hist.pdf", dir.ref)
+graf <- histogramas.clima (tmax.vars, 0.5, "ZVH_tmax_hist.pdf", dir.ref)
 
 # Temperatura mínima
-graf <- histogramas.clima (tmin.vars, "ZVH_tmin_hist.pdf", dir.ref)
+graf <- histogramas.clima (tmin.vars, 0.5, "ZVH_tmin_hist.pdf", dir.ref)
 
