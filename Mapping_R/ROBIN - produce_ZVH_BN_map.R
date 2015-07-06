b82a7b546088fb170aeb22551b8d50825a41032d
MiguelEq <equihuam@gmail.com>
2015-07-06 17:19:31 -0500

    Función para crear mapa de ZVH-BN de la salida de NETICA

--- Mapping_R/ROBIN - produce_ZVH_BN_map.R
+++ Mapping_R/ROBIN - produce_ZVH_BN_map.R
@@ -15,50 +15,50 @@ mapa.ref <- raster(arch.ref)
 gama.valores <- range(values(mapa.ref),na.rm = T)
 plot(mapa.ref)
 
-# read data table with NETICA data
-dir.ref <- paste(dir.ref,"/2004", sep = "")
-arch.netica  <- dir(dir.ref, pattern = "out.+\\.csv$")
-datos.netica.c1 <- read.csv(header = T, sep ="\t", paste(dir.ref, 
-                            arch.netica[grepl("c1", arch.netica)], sep="/"))
-datos.netica.map <- read.csv(header = T, sep ="\t", paste(dir.ref, 
-                            arch.netica[grepl("map", arch.netica)], sep="/"))
+Lee.zvh_rb.netica <- function (dir.base, nombre.salida, archivo.datos, 
+                               archivo.ref.geografica, raster.ref)
+{
+  # read data table with NETICA data
+  datos <- read.csv(paste(dir.base, archivo.datos, sep="/"), header = T, sep ="\t")
+  
+  # Clean the names of the variables
+  names.variables <- names(datos) 
+  names.variables <- unlist(lapply(names.variables, function (name) 
+            {l = strsplit(name,"\\.");l = l[[1]][length(l[[1]])]; unlist(l)}))
+  names(datos) <- names.variables
+  
+  # Identify ZVH_BN with maximum probability
+  datos$ZVH_BN <- apply(datos[,3:13], 1, function(x) which.max(x))
+  
+  # preparare map data for mapping
+  # Read coordinates from the map file used to produced the NETICA results
+  datos[,1:2] <- archivo.ref.geografica@coords
+  coordinates(datos) <- ~ x + y
+  gridded(datos) <- T
 
-# Clean the names of the variables
-names.variables <- names(datos.netica.c1) 
-names.variables <- unlist(lapply(names.variables, function (name) 
-          {l = strsplit(name,"\\.");l = l[[1]][length(l[[1]])]; unlist(l)}))
-names(datos.netica.c1) <- names.variables
-names(datos.netica.map) <- names.variables
+  # Generate the ZHV_BN map
+  netica.map <- raster(datos[,"ZVH_BN"])
+  projection(netica.map) <- projection(raster.ref)
+  
+  # Create the Geo-tiff for GIS use
+  map.arch <-paste(dir.base, nombre.salida, sep="/")
+  rf <- writeRaster(netica.map, filename=map.arch, 
+                    format="GTiff", overwrite=TRUE)
+  return (datos)
+}
 
-# Identify ZVH_BN with maximum probability
-datos.netica.c1$ZVH_BN <- apply(datos.netica.c1[,3:13], 1, 
-                                 function(x) which.max(x))
-datos.netica.map$ZVH_BN <- apply(datos.netica.map[,3:13], 1, 
-                                 function(x) which.max(x))
+# Location of NETICA files
+dir.work.clima <- paste(dir.ref,"/2004", sep = "")
+arch.netica  <- dir(dir.ref, pattern = ".+4_lon\\.csv$")
 
-# preparare map data for mapping
-# Read coordinates from the map file used to produced the NETICA results
-datos.netica.map[,1:2] <- climat.2.df@coords
-head(datos.netica.map)
-coordinates(datos.netica.map) <- ~ x + y
-gridded(datos.netica.map) <- T
-head(datos.netica.map)
+# With reference map "bov_cbz_km2" and dir base: "2004"
+Lee.zvh_rb.netica(dir.work.clima, "ZVH.tif", arch.netica, climat.2.df, mapa.ref)
 
 # Put all variables and estimates of ZVH together and write the table
-datos.zvh.clima <- cbind(climat.2.df@data, datos.netica.map@data)
-coordinates(datos.zvh.clima) <- coordinates(climat.2.df)
-opt.scipen <- getOption("scipen")
-options(scipen=999) # Avoid printing in scientific notation
-write.table(cbind(coordinates(datos.zvh.clima), datos.zvh.clima@data), sep=",",  
-            file = paste(dir.ref, "/ZVH-clima-mx.csv", sep=""), row.names = FALSE)
-options(scipen=opt.scipen)
-
-# Generate the ZHV_BN map
-rast_netica.map <- raster(datos.netica.map[,"ZVH_BN"])
-projection(rast_netica.map) <- projection(mapa.ref)
-
-# Create the Geo-tiff for GIS use
-map.arch <-paste(dir.ref, "ZVH_RB_Mex.tif", sep="/")
-rf <- writeRaster(rast_netica.map, filename=map.arch, 
-                  format="GTiff", overwrite=TRUE)
-
+#datos.zvh.clima <- cbind(climat.2.df@data, datos.netica.map@data)
+#coordinates(datos.zvh.clima) <- coordinates(climat.2.df)
+#opt.scipen <- getOption("scipen")
+#options(scipen=999) # Avoid printing in scientific notation
+#write.table(cbind(coordinates(datos.zvh.clima), datos.zvh.clima@data), sep=",",  
+#            file = paste(dir.ref, "/ZVH-clima-mx.csv", sep=""), row.names = FALSE)
+#options(scipen=opt.scipen)
