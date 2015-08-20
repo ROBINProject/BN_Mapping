d283a9c1261615e562405982773c83e941d69d9a
MiguelEq <equihuam@gmail.com>
2015-08-19 19:30:51 -0500

    Ajuste para leer los tifs requeridos por la red en turno

--- Mapping_python/Generate_IE_map.py
+++ Mapping_python/Generate_IE_map.py
@@ -29,30 +29,7 @@ def lista_nodos_diccionario(BNet):
     return node_names
 
 
-def process_on_data_table(lic_arch, net_file_name):
-    print "Welcome to Netica API for COM with Python!"
-
-    # Vincula la interface COM de NETICA y activa la aplicación
-    nt = Dispatch("Netica.Application")
-
-    # Lee la licensia de activasión de Netica
-    lic_arch = lic_arch + "/inecol_netica.txt"
-    licencia = open(lic_arch, "rb").read()
-    nt.SetPassword(licencia)
-    # Window status could be: Regular, Minimized, Maximized, Hidden
-    nt.SetWindowPosition(status="Regular")
-
-    # Display NETICA version
-    print "Using Netica version " + nt.VersionString
-
-    # Prepare the stream to read requested BN
-    streamer = nt.NewStream(net_file_name)
-
-    # ReadBNet 'options' can be one of "NoVisualInfo", "NoWindow",
-    # or the empty string (means create a regular window)
-    net = nt.ReadBNet(streamer, "")
-    node_lst = lista_nodos_diccionario(net)
-
+def process_on_data_table(lic_arch, net_file_name, data):
     #data_table = np.zeros((1, len(node_lst)))
     names = [n for n in node_lst.keys() if n not in ["zz_delt_vp"]]
     formats = []
@@ -78,14 +55,15 @@ def process_on_data_table(lic_arch, net_file_name):
     # Se libera el espacio de momoria usado por la red
     net.Delete()
 
+
 def on_toy():
     list_users = os.listdir("c:/users")
-    if "miguel.equihua" in list_users:
+    if u"miguel.equihua" in list_users:
         equipo = "esc_m"
-    elif "miguel eq" in list_users:
+    elif u"Miguel" in list_users:
         equipo = "lap_m"
     else:
-        print "hay que especificar la ubicación de los archivos necesarios:"
+        print "Hay que especificar la ubicación de los archivos necesarios:"
         print "     licencia de Netica"
         print "     Archivos tif de variables"
         print "     Red entrenada"
@@ -107,14 +85,42 @@ else:
                 u"Google Drive/2 Proyectos/RoBiN"
 
 dir_datos = u"". join([u"/Datos RoBiN/México/0_Vigente/GIS/",
-                      "Mapas_base/2004/train_data_pack/"])
+                      u"Mapas_base/2004/train_data_pack/"])
 
+# Activate Netica and Read BN
+print "Welcome to Netica API for COM with Python!"
 
-dir_tif = re.sub("2004/train_data_pack/", "", dir_robin + dir_datos)
-files = [f for f in os.listdir(dir_tif) if re.findall("tif$", f)]
+# Vincula la interface COM de NETICA y activa la aplicación
+nt = Dispatch("Netica.Application")
+
+# Lee la licensia de activasión de Netica
+lic_arch = netica_dir + "/inecol_netica.txt"
+licencia = open(lic_arch, "rb").read()
+nt.SetPassword(licencia)
+# Window status could be: Regular, Minimized, Maximized, Hidden
+nt.SetWindowPosition(status="Regular")
+
+# Display NETICA version
+print "Using Netica version " + nt.VersionString
+net_file_name = dir_robin + dir_datos +\
+ "EI_Naive_Step_n10_zvh31_peinada_sin_medias.neta"
+# Prepare the stream to read requested BN
+streamer = nt.NewStream(net_file_name)
+
+# ReadBNet 'options' can be one of "NoVisualInfo", "NoWindow",
+# or the empty string (means create a regular window)
+net = nt.ReadBNet(streamer, "")
+node_lst = lista_nodos_diccionario(net)
+
+dir_tif = re.sub(u"train_data_pack/", "", dir_robin + dir_datos)
+files = {re.sub(".tif", "", re.sub("2004_", "", f1)): f1
+         for f1 in os.listdir(dir_tif) if re.findall("tif$", f1)}
+for f in files.keys():
+    if f not in node_lst:
+        files.pop(f)
 
 # read one image to get metadata
-dataset, rows, cols, bands = readtif(dir_tif + files[0])
+dataset, rows, cols, bands = readtif(dir_tif + files.values()[0])
 
 # image metadata
 projection = dataset.GetProjection()
@@ -122,17 +128,19 @@ transform = dataset.GetGeoTransform()
 driver = dataset.GetDriver()
 
 # prediction table (pixels are rows, columns are variables)
-data_table = np.zeros((cols * rows, len(files)))
+names = files.keys()
+formats = ["float" for n in names]
+types ={"names": names, "formats": formats}
+data_table = np.zeros((cols * rows, len(files)), dtype = types)
 
 for b in xrange(len(files)):
-    image, rows, cols, bands = readtif(dir_tif + files[b])
-    band = image.GetRasterBand(b + 1)
+    image, rows, cols, bands = readtif(dir_tif + files.values()[b])
+    band = image.GetRasterBand(1)
     band = band.ReadAsArray(0, 0, cols, rows).astype(float)
     data_table[:, b] = np.ravel(band)
 
 ### insert process to predict using data_table
-net_file_name = dir_robin + dir_datos + "BN_test_python.neta"
-ie_map = process_on_data_table(netica_dir, net_file_name)
+ie_map = process_on_data_table(netica_dir, net_file_name, data_table)
 
 ### write ie_map to disk
 output_name = dir_tif + "filename.tif"
